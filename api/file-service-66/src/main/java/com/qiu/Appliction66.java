b432403f9383b036acd95686256dbe7dde8f937a
QIU <24917456@qq.com>
2024-07-20 17:42:17 +0800

    Initialize project

---
+++ api/file-service-66/src/main/java/com/qiu/Appliction66.java
@@ -0,0 +1,14 @@
+package com.qiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Q
+ */
+@SpringBootApplication
+public class Appliction66 {
+    public static void main(String[] args) {
+        SpringApplication.run(Appliction66.class,args);
+    }
+}
---
+++ api/main-service-77/src/main/java/com/qiu/Application77.java
@@ -0,0 +1,11 @@
+package com.qiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application77 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application77.class,args);
+    }
+}
