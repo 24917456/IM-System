3b56a8f837da716d33ba92847b1e6f41c61690d1
QIU <24917456@qq.com>
2024-07-22 10:07:28 +0800

    Initialize project

--- api/file-service-66/src/main/java/com/qiu/Appliction66.java
+++ api/file-service-66/src/main/java/com/qiu/Appliction66.java
@@ -2,11 +2,14 @@ package com.qiu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Q
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class Appliction66 {
     public static void main(String[] args) {
         SpringApplication.run(Appliction66.class,args);
